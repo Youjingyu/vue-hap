174ea116adc172137cb3273f6e96f1e563fce820
Youjingyu <735284268@qq.com>
2018-04-09 16:51:01 +0800

    update default route

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -7,13 +7,13 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Index',
+    //   component: Index
+    // },
     {
       path: '/',
-      name: 'Index',
-      component: Index
-    },
-    {
-      path: '/TodoMVC',
       name: 'TodoMVC',
       component: TodoMVC
     }
