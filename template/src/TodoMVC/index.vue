1c375234070d8c8a06d8ec30dbb815292bc46ff8
Youjingyu <735284268@qq.com>
2018-04-11 17:21:50 +0800

    避免转义数据绑定

--- template/src/TodoMVC/index.vue
+++ template/src/TodoMVC/index.vue
@@ -1,31 +1,25 @@
 <template>
-  <!-- <section class="todoapp" v-cloak> -->
   <section class="todoapp">		
 			<header class="header">
 				<h1 class="title"><span class="title-text">Todos</span></h1>
-				<!-- <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addTodo"> -->
 				<div class="new-todo-head">
 					<input class="new-todo" placeholder="What needs to be done?" v-model="newTodo.text">
 					<button @click="addTodo" class="new-todo-btn">add</button>
 				</div>
 			</header>
 			<section class="main" v-show="todos.length">
-				<!-- <input class="toggle-all" type="checkbox" v-model="allDone" @change="allDoneChange"> -->
 				<ul class="todo-list">
 					<li class="todo" v-for="(todo, i) in showTodos" :key="i">
 						<div class="view">
-							<!-- <input class="toggle" type="checkbox" v-model="todo.completed"> -->
-							<!-- <label @dblclick="editTodo(todo)">{{todo.title}}</label> -->
 							<input type="checkbox" v-model="todo.completed" @click="toggleComplete">
-							<div class="view-content" @click="focusItem(i)"><span :class="{'view-content-text': todo.completed}">{{todo.title}}</span></div>
+							<div class="view-content" @click="focusItem(i)"><span :class="{'view-content-text': todo.completed}">\{{todo.title}}</span></div>
 							<div class="destroy"><span v-show="focusIndex === i" @click="removeTodo(todo)" class="destroy-icon">x</span></div>
 						</div>
-						<!-- <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)"> -->
 					</li>
 				</ul>
 			</section>
 			<div class="footer">
-					<p><span class="left-item">{{leftItems}} items left</span></p>
+					<p><span class="left-item">\{{leftItems}} items left</span></p>
 					<div class="toggle-content">
 						<button @click="swicth('all')" class="toggle">all</button>
 						<button @click="swicth('active')" class="toggle">active</button>
