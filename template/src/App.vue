34358317b54fa9593a402b0322dc847f02e9956b
Youjingyu <735284268@qq.com>
2018-04-12 09:27:03 +0800

    更新模板

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <div id="app">
+    {{#router}}
     <router-view/>
+    {{else}}
+    <TodoMVC/>
+    {{/router}}
   </div>
 </template>
+
+<script>
+{{#unless router}}
+import TodoMVC from './TodoMVC/index'
+{{/unless}}
+export default {
+  name: 'App'{{#router}}{{else}},
+  components: {
+    TodoMVC
+  }{{/router}}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Index from '@/Index/index'
+import Index from '@/Index/index'
 import TodoMVC from '@/TodoMVC/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Index',
-    //   component: Index
-    // },
     {
       path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/TodoMVC',
       name: 'TodoMVC',
       component: TodoMVC
     }
