3073d1cec3f3a7a8668e8b8530ea99da7e30b313
Youjingyu <735284268@qq.com>
2018-04-13 21:06:46 +0800

    添加app.vue

---
+++ template/src/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    {{#router}}
+    <router-view/>
+    {{else}}
+    <TodoMVC/>
+    {{/router}}
+  </div>
+</template>
+
+<script>
+{{#unless router}}
+import TodoMVC from './pages/TodoMVC/index'
+{{/unless}}
+export default {
+  name: 'App'{{#router}}{{else}},
+  components: {
+    TodoMVC
+  }{{/router}}
+}
+</script>
+
+<style>
+html, body{
+  margin: 0;
+  height: 100%;
+}
+#app{
+  height: 100%;
+}
+</style>
