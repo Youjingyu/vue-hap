5bb1fb46a38c478a93e0a900b0403cf61a28eab3
Youjingyu <735284268@qq.com>
2018-04-13 16:57:56 +0800

    feat:添加启动命令以及说明

--- utils/index.js
+++ utils/index.js
@@ -75,10 +75,10 @@ To get started:
   ${yellow(
     `${data.inPlace ? '' : `cd ${data.destDirName}\n  `}${installMsg(
       data
-    )}${lintMsg(data)}npm run dev`
+    )}${lintMsg(data)}npm run dev\nor\nnpm run qa-dev`
   )}
   
-Documentation can be found at https://vuejs-templates.github.io/webpack
+Documentation can be found at https://github.com/Youjingyu/vue-hap-tools
 `
   console.log(message)
 }
